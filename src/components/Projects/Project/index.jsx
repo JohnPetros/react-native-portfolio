5aa0d7a1dd2fa8d1e3930922b50a7655a4345a29
João Pedro <joaopedro.nc@outlook.com>
2022-10-24 00:56:11 -0300

    Update index.jsx

--- src/components/Projects/Project/index.jsx
+++ src/components/Projects/Project/index.jsx
@@ -80,7 +80,7 @@ export default function Project({
           {images.map((image, index) => (
             <img
               key={index}
-              src={"/assets/" + image}
+              src={image}
               alt="Screenshot do projeto"
               className={index === currentImage ? "image active" : "image"}
               onMouseEnter={() => {
