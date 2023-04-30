a9ed6687572a1673b305fad7fd59a4ec09c63d20
João Pedro <joaopedro.nc@outlook.com>
2023-04-30 13:11:13 -0300

    :iphone: Making header and bio responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,22 @@
-import "./style.css"
+import "./style.css";
 
 export default function Header() {
   return (
-    <>
-      <header>
-        <nav className="nav-bar">
-          <h1>Portfólio Mobile</h1>
-          <ul className="list">
-            <li className="item"><a href="#bio">Bio</a></li>
-            <li className="item"><a href="#tech-stack">Ferramentas utilizadas</a></li>
-            <li className="item"><a href="#projects">Projetos</a></li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header>
+      <nav className="nav-bar">
+        <h1>Portifólio Mobile</h1>
+        <ul className="list">
+          <li className="item">
+            <a href="#bio">Bio</a>
+          </li>
+          <li className="item">
+            <a href="#tech-stack">Ferramentas utilizadas</a>
+          </li>
+          <li className="item">
+            <a href="#projects">Projetos</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
